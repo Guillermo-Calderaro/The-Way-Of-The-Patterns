0d25bb53b07e34e23bbbed6f8a547658c9f117fc
Guillermo Calderaro <guillermo.calderaro@globant.com>
2023-02-09 12:02:48 -0300

    Replace String for StringBufer

--- MoreFunRefactors/src/main/java/SquashClub.java
+++ MoreFunRefactors/src/main/java/SquashClub.java
@@ -6,56 +6,56 @@ import java.util.stream.Collectors;
 public class SquashClub {
     private List<Match> matchList = new ArrayList<>();
 
-    public String displayPlayerPointsAtAGivenDate(LocalDate date){
-        String result = "";
+    public StringBuffer displayPlayerPointsAtAGivenDate(LocalDate date){
+        StringBuffer result = new StringBuffer();
         List<Match> matchsOfTheDate = matchList.stream().filter(match -> date.equals(match.getDate()) ).collect(Collectors.toList());
 
         for (Match match: matchsOfTheDate) {
             int totalGames = 0;
-            result += "Match:\n";
+            result.append("Match:\n");
             Player player1 = match.getPlayer1();
-            result += "Player " + player1.getName() +" Score:\n";
+            result.append("Player " + player1.getName() +" Score:\n");
             for (Integer gamePoints: match.pointsOfPlayer(player1)) {
-                result += gamePoints;
+                result.append(gamePoints);
                 totalGames += gamePoints;
             }
-            result += "Match Points:";
+            result.append("Match Points:");
             switch (player1.getZone()){
                 case "A":
-                    result += totalGames *2;
+                    result.append(totalGames *2);
                     break;
                 case "B":
-                    result += totalGames;
+                    result.append(totalGames);
                     break;
                 case "C":
                     if (match.winner().equals(player1)){
-                        result += totalGames;
+                        result.append(totalGames);
                     }else{
-                        result += 0;
+                        result.append(0);
                     }
                     break;
             }
 
             Player player2 = match.getPlayer2();
             totalGames = 0;
-            result += "Player " + player2.getName() +" Score:\n";
+            result.append("Player " + player2.getName() +" Score:\n");
             for (Integer gamePoints: match.pointsOfPlayer(player2)) {
-                result += gamePoints;
+                result.append(gamePoints);
                 totalGames += gamePoints;
             }
-            result += "Match Points:";
+            result.append("Match Points:");
             switch (player2.getZone()){
                 case "A":
-                    result += totalGames *2;
+                    result.append(totalGames *2);
                     break;
                 case "B":
-                    result += totalGames;
+                    result.append(totalGames);
                     break;
                 case "C":
                     if (match.winner().equals(player2)){
-                        result += totalGames;
+                        result.append(totalGames);
                     }else{
-                        result += 0;
+                        result.append(0);
                     }
                     break;
             }
